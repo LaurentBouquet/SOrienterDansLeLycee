71fef09c5be93883210db19aa6cfc47025d6c3e1
KLenesley <lenesley.kylian@ik.me>
2026-01-12 13:49:58 +0100

    ajout bootstrap et fontawesome

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './styles/app.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import './stimulus_bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
